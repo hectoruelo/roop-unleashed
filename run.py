5f6b2571aabdacc588497fa010e71692048b6abf
hectoruelo <hectoruelo@gmail.com>
2024-01-15 13:35:27 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
-
 from roop import core
+import subprocess
+import time
+
+# Función que ejecuta tu programa en segundo plano
+def ejecutar_programa():
+    subprocess.Popen(["python", "test.py"])
+
+# Ejecutar la función en segundo plano
+ejecutar_programa()
+
 
 if __name__ == '__main__':
     core.run()
